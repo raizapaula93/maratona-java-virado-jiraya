2804df412439d832258411ab75136730cf310536
Raiza de Paula <raizapaula93@gmail.com>
2022-10-04 21:45:04 -0300

    aula45 - metodospt02Parametros

---
+++ src/main/java/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,14 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora= new Calculadora();
+        calculadora.multiplicaDoisNumeros(10,20);
+
+    }
+
+
+
+}
