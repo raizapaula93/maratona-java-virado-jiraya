01580b9ccfa4cb6e652db40d9730df5de32f9564
Raiza de Paula <raizapaula93@gmail.com>
2022-10-28 19:56:09 -0300

    aula60 - OO - Blocos de inicialização

---
+++ src/main/java/javacore/Eblocosinicializacao/test/Anime01.java
@@ -0,0 +1,12 @@
+package javacore.Eblocosinicializacao.test;
+
+
+import javacore.Eblocosinicializacao.domain.Anime;
+
+public class Anime01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        System.out.println(anime.getEpisodios());
+
+    }
+}
