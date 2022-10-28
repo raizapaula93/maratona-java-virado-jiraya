1cb94720036896c9b44bcba5b9f605213ee8c64c
Raiza de Paula <raizapaula93@gmail.com>
2022-10-28 15:16:05 -0300

    aula54 - OO - private,get e set pt01

---
+++ src/main/java/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,18 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {//código altamente coeso -> poucas coisas feitas pelo msm código
+    //quanto uma classe conhece da outra
+    //colocar o this se tiver outra variável com o msm nome passada como argumento
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+
+        //pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+
+}
