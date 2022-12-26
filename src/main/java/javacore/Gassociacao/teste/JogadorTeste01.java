945058616be0a3de76f731aef64251c5e0a66771
Raiza de Paula <raizapaula93@gmail.com>
2022-12-26 10:55:36 -0300

    aula61 - 64 -  Associação pt 01 - Arrays com Objetos

---
+++ src/main/java/javacore/Gassociacao/teste/JogadorTeste01.java
@@ -0,0 +1,17 @@
+package javacore.Gassociacao.teste;
+
+import javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTeste01 {
+    public static void main(String[] args) {
+        Jogador jogador1= new Jogador("Pelé");
+        Jogador jogador2= new Jogador("Romário");
+        Jogador jogador3= new Jogador("Cafu");
+        Jogador[] jogadores = {jogador1,jogador2,jogador3};
+
+        for (Jogador jogador: jogadores) {
+            jogador.imprime();
+        }
+
+    }
+}
