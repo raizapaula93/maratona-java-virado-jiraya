4fdfeeb43e53335a4963dd166749d50355d79da1
Raiza de Paula <raizapaula93@gmail.com>
2023-02-26 14:33:18 -0300

    aula164 -  Equals e hashcode

---
+++ src/main/java/javacore/Ycolecoes/test/EqualsTest01.java
@@ -0,0 +1,11 @@
+package javacore.Ycolecoes.test;
+
+import javacore.Ycolecoes.dominio.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "Pixel");
+        System.out.println(s1.equals(s2));
+    }
+}
